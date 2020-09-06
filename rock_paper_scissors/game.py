f104445071d1f3818793b7311e4e56fdb6bac8b4
m5cott <mcscottbsn@gmail.com>
2020-09-06 15:19:43 -0500

    Fixed game logic, added rules, deleted checks.

--- rock_paper_scissors/game.py
+++ rock_paper_scissors/game.py
@@ -2,14 +2,13 @@
     Rock Paper Scissors
     Creator: Michael Scott
 '''
-import sys
 from random import randrange
 
 game_options = ["rock", "paper", "scissors"]
 
 def welcome():
     print("Welcome to my Rock Paper Scissors game written in Python")
-    print("Here are your choices")
+    print("Here are your choices\n")
     '''
         Managing exception errors with try and except statements
         It will try to get an int input from user 1, 2, or 3
@@ -22,9 +21,6 @@ def welcome():
         print("Invalid input. Please try again.")
         welcome()
             
-    #print("Invalid Input. Please try again.")
-    #welcome()
-
 def start(user_input):
     if user_input in range(1,4):
         if user_input == 1:
@@ -36,12 +32,11 @@ def start(user_input):
     else:
         print("Your input was " + user_input + ". You'll have to try again.")
         welcome()
-    # print(choices(game_options))
 
 def play():
     # computer chooses from game option list
     computer_choice = randrange(0,3)
-    print("This is a random number " + str(computer_choice))
+
     # user's choice
     print("Your choices are:\n")
     user_choice = int(input("\t1 = rock\n\t2 = paper\n\t3 = scissors\n"))
@@ -65,26 +60,35 @@ def play():
 def logic(user_logic, game_logic):
     if user_logic == game_logic:
         print("It's a tie. The computer chose " + game_options[game_logic] + " and you chose " + game_options[user_logic])
+        welcome()
     
     if user_logic == 0: # Rock
         if game_logic - 2 != 0:
             print("You lose! " + game_options[game_logic] + " beats " + game_options[user_logic])
+            welcome()
         else:
-            print("You win! " + game_options[user_logic] + " beats " + game_options[game_logic])    
+            print("You win! " + game_options[user_logic] + " beats " + game_options[game_logic])
+            welcome()    
     elif user_logic == 1: # Paper
         if user_logic < game_logic:
             print("You lose! " + game_options[game_logic] + " beats " + game_options[user_logic])
+            welcome()
         else:
             print("You win! " + game_options[user_logic] + " beats " + game_options[game_logic])
+            welcome()
     elif user_logic == 2: # Scissors
         if user_logic - 1 == game_logic:
             print("You win! " + game_options[user_logic] + " beats " + game_options[game_logic])
+            welcome()
         else:
             print("You lose! " + game_options[game_logic] + " beats " + game_options[user_logic])
+            welcome()
         
 
 def rules():
-    print("Testing out funciton.")
+    print("Here are the rules...\n")
+    print("\tRock crushes scissors\n\tPaper covers rock\n\tScissors cuts paper\n")
+    welcome()
 
 
 def quit_game():
